25790e1facfdc5a5ec1a279d9e6dc863484ef0d8
MPotono@gmail.com <MPotono@gmail.com>
2021-11-13 16:19:12 +0100

    Dodano drugie podstawowe gui

---
+++ guiTkinter.py
@@ -0,0 +1,145 @@
+'''
+Podstawowe gui stworzone przy pomocy biblioteki Tkinter. Brak możliwości wizualizacji przy pomocy OpenGl
+Możliwość wykonania kody przy pomocy wydajnego kompilatora JIT PYPY3
+'''
+from tkinter import *
+
+import parameters
+import KMCmodel.engine
+
+class MainWindow():
+    def __init__(self) -> None:
+        
+        def queue_up_simulations():
+            simulations_list.append(make_parameters_for_simulation())
+            number_of_simulations = int(Queued_simulations.cget('text').split('=')[1])
+            number_of_simulations += 1
+            Queued_simulations['text'] = 'Number of queued simulations = ' + str(number_of_simulations)
+
+        def make_parameters_for_simulation():
+            simulation_parameters = parameters.Parameters()
+
+            simulation_parameters.create_new_parameters(space_size_entry.get(), substrate_temperature_entry.get(),melting_temperature_entry.get(),
+            boltzman_constant_entry.get(),vibration_frequency_entry.get(),n_entry.get(),energyAA_entry.get(),energy_vapour_entry.get(),
+            deposition_rate_diffusion_entry.get(),cell_dim_entry.get(),nano_second_entry.get(),deposition_rate_entry.get())
+
+            return simulation_parameters
+
+        def start_simulations():
+                        
+            if len(simulations_list) == 0:
+
+                make_parameters_for_simulation()
+                print('Uruchamianie programu:')
+                engine = KMCmodel.engine.Engine(make_parameters_for_simulation())
+
+                engine.startCalculations()
+
+                print('Ukończono')
+
+            elif len(simulations_list) > 0:
+                print('Uruchamianie zakolejkowanych symulacji')
+                for parameter in simulations_list:
+                    engine = KMCmodel.engine.Engine(parameter)
+                    engine.startCalculations()
+                print('Ukończono')
+                simulations_list.clear()
+                Queued_simulations['text'] = 'Number of queued simulations = 0'
+
+        root = Tk()
+
+        simulation_parameters = parameters.Parameters()
+        simulations_list = []
+
+        #Creating labes
+        space_size_label = Label(root, text='Space size')
+        substrate_temperature_label = Label(root, text='Substrate temperature')
+        melting_temperature_label = Label(root, text='Melting temperature')
+        boltzman_constant_label = Label(root, text='Boltzman contstant')
+        vibration_frequency_label = Label(root, text='Vibration frequency') 
+        n_label = Label(root, text='n')
+        energyAA_label = Label(root, text='EnergyAA')
+        energy_vapour_label = Label(root, text='Energy vapour')
+        deposition_rate_diffusion_label = Label(root, text='Deposition rate diffusion')
+        cell_dim_label = Label(root, text='Cell dim')
+        nano_second_label = Label(root, text='Nano second')
+        deposition_rate_label = Label(root, text='Deposition rate')
+
+
+        space_size_label.grid(column=0,row=0)
+        substrate_temperature_label.grid(column=0,row=1)
+        melting_temperature_label.grid(column=0,row=2)
+        boltzman_constant_label.grid(column=0,row=3)
+        vibration_frequency_label.grid(column=0,row=4)
+        n_label.grid(column=0,row=5)
+        energyAA_label.grid(column=0,row=6)
+        energy_vapour_label.grid(column=0,row=7)
+        deposition_rate_diffusion_label.grid(column=0,row=8)
+        cell_dim_label.grid(column=0,row=9)
+        nano_second_label.grid(column=0,row=10)
+        deposition_rate_label.grid(column=0,row=11)
+
+        #Creating text fields
+        text_fields_width = 25
+
+        space_size_entry = Entry(root, width=text_fields_width)
+        space_size_entry.insert(0, str(simulation_parameters.space_size))
+
+        substrate_temperature_entry = Entry(root, width=text_fields_width)
+        substrate_temperature_entry.insert(0, str(simulation_parameters.substrate_temperature))
+
+        melting_temperature_entry = Entry(root, width=text_fields_width)
+        melting_temperature_entry.insert(0, str(simulation_parameters.melting_temperature))
+
+        boltzman_constant_entry = Entry(root, width=text_fields_width)
+        boltzman_constant_entry.insert(0, str(simulation_parameters.boltzman_constant))
+
+        vibration_frequency_entry = Entry(root, width=text_fields_width)
+        vibration_frequency_entry.insert(0, str(simulation_parameters.vibration_frequency))
+
+        n_entry = Entry(root, width=text_fields_width)
+        n_entry.insert(0, str(simulation_parameters.n))
+
+        energyAA_entry = Entry(root, width=text_fields_width)
+        energyAA_entry.insert(0, str(simulation_parameters.energyAA))
+
+        energy_vapour_entry = Entry(root, width=text_fields_width)
+        energy_vapour_entry.insert(0, str(simulation_parameters.energy_vapour))
+
+        deposition_rate_diffusion_entry = Entry(root, width=text_fields_width)
+        deposition_rate_diffusion_entry.insert(0, str(simulation_parameters.deposition_rate_diffusion))
+
+        cell_dim_entry = Entry(root, width=text_fields_width)
+        cell_dim_entry.insert(0, str(simulation_parameters.cell_dim))
+
+        nano_second_entry = Entry(root, width=text_fields_width)
+        nano_second_entry.insert(0, str(simulation_parameters.nano_second))
+
+        deposition_rate_entry = Entry(root, width=text_fields_width)
+        deposition_rate_entry.insert(0, str(simulation_parameters.deposition_rate))
+
+        space_size_entry.grid(column=1,row=0)
+        substrate_temperature_entry.grid(column=1,row=1)
+        melting_temperature_entry.grid(column=1,row=2)
+        boltzman_constant_entry.grid(column=1,row=3)
+        vibration_frequency_entry.grid(column=1,row=4)
+        n_entry.grid(column=1,row=5)
+        energyAA_entry.grid(column=1,row=6)
+        energy_vapour_entry.grid(column=1,row=7)
+        deposition_rate_diffusion_entry.grid(column=1,row=8)
+        cell_dim_entry.grid(column=1,row=9)
+        nano_second_entry.grid(column=1,row=10)
+        deposition_rate_entry.grid(column=1,row=11)
+
+        #Creating buttons
+        start_simulations_button = Button(root, text='Start simulations', command = start_simulations)
+        queue_up_simulation_button = Button(root, text='Queue up simulation', command = queue_up_simulations)
+
+        start_simulations_button.grid(column=0, row=12)
+        queue_up_simulation_button.grid(column=1, row=12)
+
+        Queued_simulations = Label(root, text='Number of queued simulations = 0')
+        Queued_simulations.grid(column=3, row=0)
+
+
+        root.mainloop()
--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 '''Main file of program'''
-
 import platform
 import sys
 import os
-
-from PyQt5.QtCore import QLine
-from KMCmodel.parameters import Parameters
 
 import parameters
 import KMCmodel.engine
@@ -32,147 +28,18 @@ def make_calculation_without_gui():
 
     print('Ukończono')
 
-def make_calculations_with_gui():
+def make_calculations_with_guiQt5():
     import PyQt5.QtWidgets as qtw
-
-    class MainWindow(qtw.QWidget):
-        def __init__(self):
-            super().__init__()
-
-            self.setLayout(qtw.QGridLayout())
-
-            simulation_parameters = parameters.Parameters()
-            simulations_list = []
-
-            #Creating labes
-            space_size_label = qtw.QLabel('Space size')
-            substrate_temperature_label = qtw.QLabel('Substrate temperature') 
-            melting_temperature_label = qtw.QLabel('Melting temperature')
-            boltzman_constant_label = qtw.QLabel('Boltzman contstant')
-            vibration_frequency_label = qtw.QLabel('Vibration frequency')  
-            n_label = qtw.QLabel('n')
-            energyAA_label = qtw.QLabel('EnergyAA')
-            energy_vapour_label = qtw.QLabel('Energy vapour') 
-            deposition_rate_diffusion_label = qtw.QLabel('Deposition rate diffusion')
-            cell_dim_label = qtw.QLabel('Cell dim')
-            nano_second_label = qtw.QLabel('Nano second')
-            deposition_rate_label = qtw.QLabel('Deposition rate')
-
-            self.layout().addWidget(space_size_label)
-            self.layout().addWidget(substrate_temperature_label)
-            self.layout().addWidget(melting_temperature_label)
-            self.layout().addWidget(boltzman_constant_label)
-            self.layout().addWidget(vibration_frequency_label)
-            self.layout().addWidget(n_label)
-            self.layout().addWidget(energyAA_label)
-            self.layout().addWidget(energy_vapour_label)
-            self.layout().addWidget(deposition_rate_diffusion_label)
-            self.layout().addWidget(cell_dim_label)
-            self.layout().addWidget(nano_second_label)
-            self.layout().addWidget(deposition_rate_label)
-
-            #Creating text fields
-            space_size_entry = qtw.QLineEdit()
-            space_size_entry.setText(str(simulation_parameters.space_size))
-
-            substrate_temperature_entry = qtw.QLineEdit()
-            substrate_temperature_entry.setText(str(simulation_parameters.substrate_temperature))
-
-            melting_temperature_entry = qtw.QLineEdit()
-            melting_temperature_entry.setText(str(simulation_parameters.melting_temperature))
-
-            boltzman_constant_entry = qtw.QLineEdit()
-            boltzman_constant_entry.setText(str(simulation_parameters.boltzman_constant))
-
-            vibration_frequency_entry = qtw.QLineEdit() 
-            vibration_frequency_entry.setText(str(simulation_parameters.vibration_frequency))
-
-            n_entry = qtw.QLineEdit()
-            n_entry.setText(str(simulation_parameters.n))
-
-            energyAA_entry = qtw.QLineEdit()
-            energyAA_entry.setText(str(simulation_parameters.energyAA))
-
-            energy_vapour_entry = qtw.QLineEdit()
-            energy_vapour_entry.setText(str(simulation_parameters.energy_vapour))
-
-            deposition_rate_diffusion_entry = qtw.QLineEdit()
-            deposition_rate_diffusion_entry.setText(str(simulation_parameters.deposition_rate_diffusion))
-
-            cell_dim_entry = qtw.QLineEdit()
-            cell_dim_entry.setText(str(simulation_parameters.cell_dim))
-
-            nano_second_entry = qtw.QLineEdit()
-            nano_second_entry.setText(str(simulation_parameters.nano_second))
-
-            deposition_rate_entry = qtw.QLineEdit()
-            deposition_rate_entry.setText(str(simulation_parameters.deposition_rate))
-
-            self.layout().addWidget(space_size_entry, 0,1)
-            self.layout().addWidget(substrate_temperature_entry, 1,1)
-            self.layout().addWidget(melting_temperature_entry, 2,1)
-            self.layout().addWidget(boltzman_constant_entry, 3,1)
-            self.layout().addWidget(vibration_frequency_entry, 4,1)
-            self.layout().addWidget(n_entry, 5, 1)
-            self.layout().addWidget(energyAA_entry, 6,1)
-            self.layout().addWidget(energy_vapour_entry, 7,1)
-            self.layout().addWidget(deposition_rate_diffusion_entry, 8,1)
-            self.layout().addWidget(cell_dim_entry, 9,1)
-            self.layout().addWidget(nano_second_entry, 10,1)
-            self.layout().addWidget(deposition_rate_entry, 11,1)
-
-            #Creating buttons
-            start_simulations_button = qtw.QPushButton('Start simulations', clicked = lambda: start_simulations())
-            queue_up_simulation_button = qtw.QPushButton('Queue up simulation', clicked = lambda: queue_up_simulations())
-
-            self.layout().addWidget(start_simulations_button, 12,0)
-            self.layout().addWidget(queue_up_simulation_button, 12,1)
-
-            Queued_simulations = qtw.QLabel('Number of queued simulations = 0')
-            self.layout().addWidget(Queued_simulations, 0,2)
-
-            def queue_up_simulations():
-                simulations_list.append(make_parameters_for_simulation())
-                number_of_simulations = int(Queued_simulations.text().split('=')[1])
-                number_of_simulations += 1
-                Queued_simulations.setText('Number of queued simulations = ' + str(number_of_simulations))
-
-            def make_parameters_for_simulation():
-                simulation_parameters = parameters.Parameters()
-
-                simulation_parameters.create_new_parameters(space_size_entry.text(), substrate_temperature_entry.text(),melting_temperature_entry.text(),
-                boltzman_constant_entry.text(),vibration_frequency_entry.text(),n_entry.text(),energyAA_entry.text(),energy_vapour_entry.text(),
-                deposition_rate_diffusion_entry.text(),cell_dim_entry.text(),nano_second_entry.text(),deposition_rate_entry.text())
-
-                return simulation_parameters
-
-            def start_simulations():
-                
-                if len(simulations_list) == 0:
-
-                    make_parameters_for_simulation()
-                    print('Uruchamianie programu:')
-                    engine = KMCmodel.engine.Engine(make_parameters_for_simulation())
-
-                    engine.startCalculations()
-
-                    print('Ukończono')
-
-                elif len(simulations_list) > 0:
-                    print('Uruchamianie zakolejkowanych symulacji')
-                    for parameter in simulations_list:
-                        engine = KMCmodel.engine.Engine(parameter)
-                        engine.startCalculations()
-                    print('Ukończono')
-                    simulations_list.clear()
-                    Queued_simulations.setText('Number of queued simulations = 0')
-
-
-            self.show()
+    from guiQt5 import MainWindow
 
     app = qtw.QApplication([])
     mw = MainWindow()
     app.exec_()
+
+def make_calculations_with_guiTkinter():
+    from guiTkinter import MainWindow
+
+    main_window = MainWindow()
 
 def main():
 
@@ -180,8 +47,8 @@ def main():
     print_machine_info()
 
     #make_calculation_without_gui()
-
-    make_calculations_with_gui()
+    #make_calculations_with_guiQt5()
+    make_calculations_with_guiTkinter()
 
     return 0
 
